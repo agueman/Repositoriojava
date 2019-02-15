343dde208756e85c2b5d923caaaa567ac6d04a96
USUARIO <USUARIO@Aula05-Pc27>
2019-02-15 13:43:40 +0100

    Subida desde clase 1.0

---
+++ pruebagit/src/pruebagit/Principal.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package pruebagit;
+
+/**
+ * @author USUARIO
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero = 5;
+		for (int i=1; i<=10; i++) {
+			System.out.println
+			(numero + " x " + i + " = " + (numero*i));
+		}
+
+	}
+
+}
